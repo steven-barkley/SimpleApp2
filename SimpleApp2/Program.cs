03171aa3344a3c6d4c29cd294c139c76fc0bfcb5
Steven Reginald Barkley <steven.barkley@outlook.com>
2018-09-01 16:25:23 -0400

    ADD to GITHIB

--- SimpleApp2/Program.cs
+++ SimpleApp2/Program.cs
@@ -10,6 +10,19 @@ namespace SimpleApp2
     {
         static void Main(string[] args)
         {
+            /// Set up console UI (CUI)
+            Console.Title = "My Rocking App";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("******************************");
+            Console.WriteLine("** Welcome to My Rocking App**");
+            Console.WriteLine("******************************");
+            Console.WriteLine("******************************");
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            // What for the Enter key to be pressed.
+            Console.ReadLine();
         }
     }
 }
+
